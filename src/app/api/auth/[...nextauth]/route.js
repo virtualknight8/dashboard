b34805cc0a8035483579e6f6f12d90b71acd8277
virtualknight8 <ankitkhola2@gmail.com>
2023-07-12 17:33:58 +0530

    clienID remove

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,8 +4,8 @@ import GoogleProvider from 'next-auth/providers/google'
 export const authOptions = {
     providers:[
         GoogleProvider({
-            clientId:'877573455221-l52j3nsv9dt41g4lgobkbanau8gt37f4.apps.googleusercontent.com',
-            clientSecret:'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'
+            clientId:'',
+            clientSecret:''
         })
     ],
     pages:{
